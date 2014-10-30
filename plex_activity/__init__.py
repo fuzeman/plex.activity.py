1640940170535e5b9be69c782a8b17cb46b4bb6d
Dean Gardiner <gardiner91@gmail.com>
2014-10-30 15:58:23 +1300

    Preparing package structure for PyPI release

--- plex_activity/__init__.py
+++ plex_activity/__init__.py
@@ -1,4 +1,14 @@
-from plex_activity import activity
+import logging
 
-# Global objects (using defaults)
-Activity = activity.Activity()
+log = logging.getLogger(__name__)
+
+__version__ = '0.6.0'
+
+
+try:
+    from plex_activity import activity
+
+    # Global objects (using defaults)
+    Activity = activity.Activity()
+except Exception, ex:
+    log.warn('Unable to import submodules - %s', ex)
